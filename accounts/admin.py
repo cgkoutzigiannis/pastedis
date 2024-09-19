d4f51e4e14a6ae2159c00b586cda4fc5e1f83e9d
Christos Gkoutzigiannis <cgkoutzigiannis@gmail.com>
2024-09-19 20:02:01 +0300

    Created custom user model. - Added a new CustomUser model. - Modified forms in the admin panel. - Changed settings needed. - Ran migrations.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUerCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUerCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ["email", "username"]
