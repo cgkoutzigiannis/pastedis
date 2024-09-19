27083125c5a99321993e2f39c05c65c4da1c14b7
Christos Gkoutzigiannis <cgkoutzigiannis@gmail.com>
2024-09-19 20:46:12 +0300

    Created login, logout and signup flow. There is still work to be done: - Password reset. - Password change. - Styling pages, with errors and tips.

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 
-class CustomUerCreationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ("username", "email")
